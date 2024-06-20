737f04b94390bf54e0b905ee0e124501294a8d69
SiRiOS <sirios@sirios.com.br>
2024-06-20 17:27:53 -0300

    Estrutura da aplicação - Configurando temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from "styled-components";
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success' | 'neutral' | 'warning';
 
 interface ButtonContainerProps {
   variant: ButtonVariant;
@@ -10,15 +10,23 @@ const buttonVariants = {
   primary: 'purple',
   secondary: 'orange',
   danger: 'red',
-  success: 'green'
+  success: 'green',
+  neutral: 'gray',
+  warning: 'yellow'
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  ${props => {
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+  /* ${props => {
     return css`
       background-color: ${buttonVariants[props.variant]}
     `
-  }}
+  }} */
 `;
